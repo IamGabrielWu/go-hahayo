cd6b0e89ae8c3e6fec393c840a9b2d2e7c8e3f71
dz.wu <dz.wu@aftership.com>
2020-03-24 23:13:31 +0800

    test

---
+++ src/tushare/README.md
@@ -0,0 +1,5 @@
+### start hello world
+```
+export token=<value in token file>
+go run main.go
+```
--- src/tushare/main.go
+++ src/tushare/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"os"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	url := "http://api.tushare.pro"
+	fmt.Println("URL:>", url)
+	token := os.Getenv("token")
+	data := "{" +
+		"	\"api_name\": \"trade_cal\", " +
+		"	\"token\": \"" + token + "\"," +
+		"	\"params\": { " +
+		"		\"exchange\":\"\"," +
+		"		\"start_date\": \"20200321\", " +
+		"		\"end_date\": \"20200323\", " +
+		"		\"is_open\": \"0\" " +
+		"		}," +
+		"		\"fields\": \"exchange,cal_date,is_open,pretrade_date\" " +
+		"}"
+
+	fmt.Println(data)
+	var jsonStr = []byte(data)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 }
